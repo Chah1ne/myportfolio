e452992f46fc1882bb6b9026a4253397e9cb8550
Chah1ne <tsouri.chahine@gmail.com>
2024-07-21 00:22:47 +0100

    5 commit

--- next.config.js
+++ next.config.js
@@ -1,14 +1,17 @@
-const path = require('path')
- 
+const path = require('path');
+
 module.exports = {
   output: "export",
   distDir: "docs",
+  basePath: '/myportfolio',  // Set this to your GitHub repository name
+  assetPrefix: '/myportfolio',  // Set this to your GitHub repository name
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
   images: {
-    domains: ["https://chah1ne.github.io/myportfolio/"],
-    path: "/myportfolio",
+    domains: ["https://chah1ne.github.io"],
+    path: "/myportfolio/_next/image",
+    loader: 'default',
     unoptimized: true,
   },
-}
+};
