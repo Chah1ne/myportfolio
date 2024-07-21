234dd6b5c4a1df7a0cf36d60cf3e263b0b1eacb2
Chah1ne <tsouri.chahine@gmail.com>
2024-07-21 13:34:29 +0100

    tt

--- next.config.js
+++ next.config.js
@@ -1,14 +1,27 @@
+// next.config.js
 const path = require('path');
+const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
-  output: "export",
-  distDir: "docs",
-  basePath: '/myportfolio',  // Set this to the repository name
-  assetPrefix: '/myportfolio/',  // Set this to the repository name
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
   images: {
-    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        pathname: '**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'media.dev.to',
+        pathname: '**',
+      },
+    ],
   },
+  basePath: isProd ? '/myportfolio' : '', // Ensure this matches your deployment path
+  assetPrefix: isProd ? '/myportfolio/' : '', // Ensure this matches your deployment path
+  output: 'export', // Static export configuration
+  distDir: 'out', // Output directory for static files
 };
