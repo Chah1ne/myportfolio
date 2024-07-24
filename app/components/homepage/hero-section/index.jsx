740e92a500162b5e4ffe61fcd39caea5df4a3857
Chah1ne <tsouri.chahine@gmail.com>
2024-07-24 12:59:55 +0100

    update profile

--- app/components/homepage/hero-section/index.jsx
+++ app/components/homepage/hero-section/index.jsx
@@ -132,6 +132,8 @@ function HeroSection() {
                 <span className="text-gray-400">{"', '"}</span>
                 <span className="text-amber-300">MongoDB</span>
                 <span className="text-gray-400">{"', '"}</span>
+                <span className="text-amber-300">sequelize</span>
+                <span className="text-gray-400">{"', '"}</span>
                 <span className="text-amber-300">Docker</span>
                 <span className="text-gray-400">{"', '"}</span>
                 <span className="text-amber-300">vercel</span>
