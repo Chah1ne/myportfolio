5542eebce67b41f80277a387afbdb78f6bbe7fbb
Chah1ne <tsouri.chahine@gmail.com>
2024-08-04 20:26:34 +0100

    'update'

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -3,20 +3,20 @@ export const educations = [
     id: 1,
     title: "software engineering student",
     duration: "sept. 2024 - present",
-    institution: "Université Sésame - Tunis",
+    institution: "Sésame University - Tunis",
     additionalInfo: "Full-Stack Software Developer",
   },
   {
     id: 1,
     title: "Bachelor's Degree in Computer Science",
     duration: "sept. 2021 - juin 2024",
-    institution: "Institut Supérieur des Etudes Technologiques de Sfax - Tunisie",
+    institution: "Higher Institute of Technological Studies of Sfax - Tunisia",
     additionalInfo: "Specialization In Information Systems Development ",
   },
   {
     id: 2,
     title: "Baccalauréat in Computer Science",
     duration: "2020-2021",
-    institution: "Lycée Bardo, Tunis - Tunisie",
+    institution: "Bardo High School, Tunis - Tunisia",
   }
 ];
