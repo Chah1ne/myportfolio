5d8c1113a9f6a4e48d8033ba21f38c399e4ce252
Chah1ne <130062330+Chah1ne@users.noreply.github.com>
2024-10-03 18:40:54 +0200

    Update personal-data.js

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -4,7 +4,7 @@ export const personalData = {
   designation: "Full-Stack Software Developer",
   description: "My name is Tsouri Chahine. I am a professional and enthusiastic Web Developer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and strive to make the web more open to the world. My core skill is based on JavaScript, and I love to do most of the things using JavaScript. Additionally, I am a skilled Mobile Developer using Flutter, which allows me to create high-quality, cross-platform mobile applications. I am available for any kind of job opportunity that suits my skills and interests.",
   email: 'tsouri.chahine@gmail.com',
-  phone: '+21699290902',
+  phone: '+216 99290902',
   address: 'Tunisie , Tunis - Bardo',
   github: 'https://github.com/Chah1ne',
   facebook: 'https://www.facebook.com/tsouriChahine/',
