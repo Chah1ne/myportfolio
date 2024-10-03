16f63f1b4f27b20beea97b27e33cdf85b80e7491
Chah1ne <130062330+Chah1ne@users.noreply.github.com>
2024-10-03 18:31:47 +0200

    Update experience.js

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,18 +1,24 @@
 export const experiences = [
-  {
+{
     id: 1,
+    title: 'Technical Consultant',
+    company: "TeamWill",
+    duration: "(oct 2024 - Today"
+  },
+  {
+    id: 2,
     title: 'Software Developer ( Flutter & Mern stack )',
     company: "ASQII – HealthTech Startup.",
     duration: "(nov 2023 - juil 2024)"
   },
   {
-    id: 2,
+    id: 3,
     title: "Full-Stack Developer",
     company: "(freelance)",
     duration: "(fev 2022 - aout 2023)"
   },
   {
-    id: 3,
+    id: 4,
     title: "Self Employed",
     company: "Code and build something in everyday.",
     duration: "(Jan 2021 -2022)"
