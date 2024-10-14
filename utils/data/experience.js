f16a9a187b3973450f3cc584e92bd26cde0825ea
Chah1ne <130062330+Chah1ne@users.noreply.github.com>
2024-10-14 12:37:58 +0200

    Update experience.js

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -7,7 +7,7 @@ export const experiences = [
   },
   {
     id: 2,
-    title: 'Software Developer ( Flutter & Mern stack )',
+    title: 'Junior Software Developer ( Flutter & Mern stack )',
     company: "ASQII – HealthTech Startup.",
     duration: "(oct 2023 - sept 2024)"
   },
